3af21c2fb11855c94c095bade6159e31d6d67ca5
Mustapha <bennay.mus@gmail.com>
2025-10-30 14:28:50 +0100

    Add test for calculator add function

---
+++ tests/test_calculatorr.py
@@ -0,0 +1,8 @@
+# ADD function to test our test skript
+from src.calculator import add
+import pytest
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-2, 2) == 0
+    assert add(0, 0) == 0
